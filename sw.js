02283b3e5bfd5c39bbe5b150766fa4423214d42f
Aibo <ggwp4236@gmail.com>
2023-05-19 18:21:39 +0400

    fix sw list

--- sw.js
+++ sw.js
@@ -4,17 +4,8 @@ self.addEventListener('install', function(event) {
         return cache.addAll([
           '/',
           '/zaysan_city/index.html',
-          '/zaysan_city/about.html',
-          '/zaysan_city/blog.html',
-          '/zaysan_city/contact.html',
-          '/zaysan_city/politic.html',
-          '/zaysan_city/portfolio.html',
-          '/zaysan_city/service.html',
-          '/zaysan_city/single.html',
-          '/zaysan_city/team.html',
-          '/zaysan_city/styles.css',
-          '/zaysan_city/script.js',
-          '/zaysan_city/manifest.json',
+          '/zaysan_city/css/style.css',
+          '/zaysan_city/js/main.js',
         ]);
       })
     );
